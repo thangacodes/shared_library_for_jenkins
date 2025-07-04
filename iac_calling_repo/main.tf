4f0e7593d3dcf8ae1c3ede0d5746d8abdaf18776
Thangadurai (Thangaa) <38905243+thangacodes@users.noreply.github.com>
2025-07-04 13:58:30 +0530

    syntax error fixed

--- iac_calling_repo/main.tf
+++ iac_calling_repo/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2-create" {
-  source   = "git::https://github.com/thangacodes/terraform.git/enterprise_level_tfmodule_calling/ec2/modules/ec2?ref=v0.0.1"
+  source   = "git::https://github.com/thangacodes/terraform/enterprise_level_tfmodule_calling/ec2/modules/ec2?ref=v0.0.1"
   image_id = "ami-0d03cb826412c6b0f"
   vm_spec  = "t2.micro"
   keyname  = "bastion"
